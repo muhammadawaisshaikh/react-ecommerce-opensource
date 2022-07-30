b2dfb3442ca0c89842830886a077771e8cf5cf6c
Muhammad Awais <awaisshaikh94@gmail.com>
2022-07-30 15:59:36 +0500

    added drawer with routes defined in routes file. few icon changes required

--- src/core/routes/index.js
+++ src/core/routes/index.js
@@ -1,17 +1,21 @@
 import Homepage from "../../pages/homepage";
 import Products from "../../pages/products";
+import SideMenu from "../../shared/sidemenu";
 
 export const routes = [
     {
         path: '/',
-        component: <Homepage />
+        name: "Side Menu",
+        component: <SideMenu />
     },
     {
         path: '/homepage',
+        name: "Home",
         component: <Homepage />
     },
     {
         path: '/products',
+        name: "Products",
         component: <Products />
     }
 ]
--- src/shared/sidemenu/index.js
+++ src/shared/sidemenu/index.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import * as React from 'react';
 import Style from './style';
+import { routes } from '../../core/routes';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function SideMenu() {
-    const classes = Style();
+  const classes = Style();
+
+  const anchor = "left";
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {routes.map((route, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ShoppingCartIcon />
+              </ListItemIcon>
+              <ListItemText primary={route.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
-    <div>
-        <h1 className={classes.highlight}>SideMenu</h1>
-    </div>
+    <React.Fragment key={anchor}>
+      <IconButton onClick={toggleDrawer(anchor, true)} >
+        <MenuIcon />
+      </IconButton>
+
+      <Drawer
+        anchor={anchor}
+        open={state[anchor]}
+        onClose={toggleDrawer(anchor, false)}
+      >
+        {list(anchor)}
+      </Drawer>
+    </React.Fragment>
   );
 }
 
