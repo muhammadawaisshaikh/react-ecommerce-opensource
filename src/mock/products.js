f23b4c0d87567663fb7cf7c909fe93824314452b
Muhammad Awais <awaisshaikh94@gmail.com>
2022-08-06 17:14:20 +0500

    moving mockdata of products to mock file

---
+++ src/mock/products.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        title: "ABC Product",
+        image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
+        price: 999,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    },
+    {
+        title: "DEF Product",
+        image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
+        price: 999,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    },
+    {
+        title: "GHI Product",
+        image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
+        price: 999,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    },
+    {
+        title: "JKL Product",
+        image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
+        price: 999,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    },
+    {
+        title: "MNO Product",
+        image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
+        price: 999,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    }
+]
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,43 +1,12 @@
 import React from 'react';
-import ProductCard from '../../shared/product-card';
-import Style from './style';
 import { Grid, Container } from '@mui/material';
+import Style from './style';
+import ProductCard from '../../shared/product-card';
+import { products } from '../../mock/products';
 
 function Products() {
   const classes = Style();
-
-  const data = [
-    {
-      title: "ABC Product",
-      image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
-      price: 999,
-      description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-    },
-    {
-      title: "DEF Product",
-      image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
-      price: 999,
-      description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-    },
-    {
-      title: "GHI Product",
-      image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
-      price: 999,
-      description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-    },
-    {
-      title: "JKL Product",
-      image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
-      price: 999,
-      description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-    },
-    {
-      title: "MNO Product",
-      image: "https://corporate.oriflame.com/about-oriflame/product-philosophy/-/media/2E279B139BD74617958EDE0360D641FC.ashx",
-      price: 999,
-      description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-    }
-  ]
+  const data = products;
 
   return (
     <div className={classes.root}>
